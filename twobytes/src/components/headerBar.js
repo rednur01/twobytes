edb60adf21b67122768639e9633ccedf34e773da
Red <rednur01@gmail.com>
2018-08-24 07:44:19 -0400

    header

--- twobytes/src/components/headerBar.js
+++ twobytes/src/components/headerBar.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import logo from './applogo.svg';
 
 class HeaderBar extends Component {
+    constructor(props) {
+        super(props);
+        this.onHome = this.onHome.bind(this);
+        this.onAbout = this.onAbout.bind(this);
+        this.onFavorite = this.onFavorite.bind(this);
+        this.onAccount = this.onAccount.bind(this);
+    }
+
+    onHome(e) {
+        e.preventDefault();
+    }
+
+    onAbout(e) {
+        e.preventDefault();
+    }
+
+    onFavorite(e) {
+        e.preventDefault();
+    }
+
+
+    onAccount(e) {
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div className="HeaderBar">
-                <img id="applogo" src={logo} alt="Logo Here"/>
+            <ul>
+                <li><a href="#" onClick={this.onHome}>Home</a></li>
+                <li><a href="#" onClick={this.onAbout}>About Us</a></li>
+                <li><img id="applogo" src={logo} alt="Logo Here"/></li>
+                <li><a href="#" onClick={this.onFavorite}>Favorites</a></li>
+                <li><a href="#" onClick={this.onAccount}>My Account</a></li>
+            </ul>
             </div>
         );
     }
